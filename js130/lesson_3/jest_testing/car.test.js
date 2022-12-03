6369f49185bd2f058387c76fea24b292fbb8a364
Deepak Karivelil <deepakkarivelil@gmail.com>
2022-12-03 14:52:17 -0400

    working through lesson 3

--- js130/lesson_3/jest_testing/car.test.js
+++ js130/lesson_3/jest_testing/car.test.js
@@ -1,17 +1,50 @@
-const Car = require("./car");
+const Car = require('./car');
 
-describe("The Car class", () => {
-  test('two new cars are equal objects', () => {
-    let car1 = new Car();
+describe('The Car class', () => {
+
+  let car;
+  beforeEach(() => {
+    car = new Car();
+  });
+
+  test('has four wheels', () => {
+    expect(car.wheels).toBe(4);
+  });
+
+  test('two newly created cars are object equal', () => {
     let car2 = new Car();
 
-    car1.a = {
-      a: 'hello',
-    };
-    car2.a = {
-      a: 'hello',
-    };
+    expect(car).toEqual(car2);
+  });
 
-    expect(car1).toEqual(car2);
-  })
+  test('a newly created car does not have doors', () => {
+    expect(car.doors).toBeUndefined();
+  });
+
+  test('raises an error when called drive on it', () => {
+    expect(() => car.drive()).toThrow();
+  });
+
+  test('raises a TypeError when called drive on it', () => {
+    expect(() => car.drive()).toThrow(TypeError);
+  });
+
+  test('a new car has no mileage info', () => {
+    expect(car.mileageInfo).toBeNull();
+  });
+
+  test('wheels is truthy', () => {
+    expect(car.wheels).toBeTruthy();
+  });
+
+  test('array contains car', () => {
+    let arr = [1, 2, 3];
+    arr.push(car);
+
+    expect(arr).toContain(car);
+  });
+
+  test('car has wheels', () => {
+    expect(car.wheels).not.toBeUndefined();
+  });
 });
