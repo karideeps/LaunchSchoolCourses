06ba3e1e99fccfeaaea5da20df416e9ce395763e
Deepak Karivelil <deepakkarivelil@gmail.com>
2023-05-17 17:39:57 -0300

    working through js185

--- js185/expense/schema.sql
+++ js185/expense/schema.sql
@@ -2,5 +2,13 @@ CREATE TABLE expenses (
   id SERIAL PRIMARY KEY,
   amount NUMERIC(6, 2) NOT NULL CHECK (amount > 0),
   memo TEXT NOT NULL,
-  create_on DATE DEFAULT NOW() NOT NULL
+  created_on DATE DEFAULT NOW() NOT NULL
 );
+
+INSERT INTO expenses
+(amount, memo)
+VALUES
+(14.56, 'Pencils'),
+(3.29, 'Coffee'),
+(49.99, 'Text Editor'),
+(3.49, 'More Coffee');
